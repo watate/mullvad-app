0bc8be457dcb5b4116fdccb42bcb995e63a6e652
Walter <walter.spades@gmail.com>
2021-02-20 10:04:29 +0800

    Update: 2021.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,16 +4,16 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #$trustedPublisherCertificate = "$toolsDir\openvpn_trusted_publisher.cer"
 #$trustedPublisherCertificateHash = '8f53adb36f1c61c50e11b8bdbef8d0ffb9b26665a69d81246551a0b455e72ec0b26a34dc9b65cb3750baf5d8a6d19896c3b4a31b578b15ab7086377955509fad'
 
-#$pgpPublicKey = "$toolsDir\MullvadVPN-2021.1.exe.asc"
+#$pgpPublicKey = "$toolsDir\MullvadVPN-2021.2.exe.asc"
 #$pgpPublicKeyHash = '03fedf10b55d10ca03a3c0e7d8139a17b19255a869de8f40a70ea70b21fab8ea'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url64bit      = 'https://mullvad.net/media/app/MullvadVPN-2021.1.exe'
+  url64bit      = 'https://mullvad.net/media/app/MullvadVPN-2021.2.exe'
 
-  checksum64    = 'bed42a7cb009ebd027194f20940b2b9856037a0e581a9bdfaf9b325b8d2f6af3'
+  checksum64    = 'a96483eecbc58da8a8dc696946300a5d6c4e8fe05f4d38ea264b41db711a77dc'
   checksumType64= 'sha256'
 
   silentArgs    = '/S'
