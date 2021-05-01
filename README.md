b47a33ef64dbe78a267401fecd63245e7f3c569c
Walter <walter.spades@gmail.com>
2021-05-01 13:48:29 +0800

    Update: 2021.3

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # testing instructions
 source: https://chocolatey.org/courses/creating-chocolatey-packages/building-testing-and-pushing
+
 run
 - choco pack
 - choco install packageName -dv -s .
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,30 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-#$trustedPublisherCertificate = "$toolsDir\openvpn_trusted_publisher.cer"
-#$trustedPublisherCertificateHash = '8f53adb36f1c61c50e11b8bdbef8d0ffb9b26665a69d81246551a0b455e72ec0b26a34dc9b65cb3750baf5d8a6d19896c3b4a31b578b15ab7086377955509fad'
-
-#$pgpPublicKey = "$toolsDir\MullvadVPN-2021.2.exe.asc"
-#$pgpPublicKeyHash = '03fedf10b55d10ca03a3c0e7d8139a17b19255a869de8f40a70ea70b21fab8ea'
-
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url64bit      = 'https://mullvad.net/media/app/MullvadVPN-2021.2.exe'
+  url64bit      = 'https://mullvad.net/media/app/MullvadVPN-2021.3.exe'
 
-  checksum64    = 'a96483eecbc58da8a8dc696946300a5d6c4e8fe05f4d38ea264b41db711a77dc'
+  checksum64    = 'c34580a8179141ea5fa9c1502b0c0784a40e93280cdcff8912f8b3190c196a17'
   checksumType64= 'sha256'
 
   silentArgs    = '/S'
 }
 
-#. "$toolsDir\utils\utils.ps1"
-
-#Write-Host "Adding OpenVPN to the Trusted Publishers (needed to have a silent install of the TAP driver)..."
-#AddTrustedPublisherCertificate -file "$trustedPublisherCertificate"
-
 Install-ChocolateyPackage @packageArgs
-
-#Write-Host "Removing OpenVPN from the Trusted Publishers..."
-#RemoveTrustedPublisherCertificate -file "$trustedPublisherCertificate"
